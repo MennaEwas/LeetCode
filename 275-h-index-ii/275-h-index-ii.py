734c4e5732269b7fe2237148718c690c74389a5e
MennaEwas <61921636+MennaEwas@users.noreply.github.com>
2022-07-10 21:59:23 +0000

    Time: 178 ms (70.99%), Space: 20.6 MB (35.38%) - LeetHub

--- 275-h-index-ii/275-h-index-ii.py
+++ 275-h-index-ii/275-h-index-ii.py
@@ -1,14 +1,14 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         n = len(citations)
-        l, r = 0, n-1
-        while l<=r:
-            mid = (l+r)//2
-            # if the index of paper is smaller than its corresponding citation value, then search space is on the left of mid
-            # n-mid is the number of papers from index mid to the last index
-            if (n-mid)<=citations[mid]:
+        l = 0
+        r = n - 1
+        
+        while l <= r: 
+            mid = (l + r) // 2
+            if (n - mid) <= citations[mid]:
                 r = mid - 1
             else:
                 l = mid + 1
-               
-        return n-l
+        return n - l
+        
