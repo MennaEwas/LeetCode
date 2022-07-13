6754b6454f7e6014ab551ee5ff247b476c31a455
MennaEwas <61921636+MennaEwas@users.noreply.github.com>
2022-07-13 15:30:14 +0000

    Time: 52 ms (46.99%), Space: 13.9 MB (52.94%) - LeetHub

--- 231-power-of-two/231-power-of-two.py
+++ 231-power-of-two/231-power-of-two.py
@@ -1,15 +1,20 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        #base case 
+        #Negative
         if n < 0:
             return False
+        #Base Case
         if n == 1 or n ==2:
             return True
+        #Recursive
         elif n > 2:
             n = n / 2
             return self.isPowerOfTwo(n)
         else:
             return False
+        
+#Complixity = lg(n) 
+#Space = lg(n)
         
             
         
