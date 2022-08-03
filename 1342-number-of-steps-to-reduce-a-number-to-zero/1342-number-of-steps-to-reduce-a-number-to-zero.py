87979f1483f7722bbf8c779870cf3771267586ea
MennaEwas <61921636+MennaEwas@users.noreply.github.com>
2022-08-03 15:55:48 +0200

    Time: 50 ms (45.01%), Space: 14 MB (8.54%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
@@ -2,7 +2,7 @@ class Solution:
     def numberOfSteps(self, num: int) -> int:
         step =  0
         while num:
-            if num%2 == 0: #even 
+            if not num%2 :
                 num /= 2
             else:
                 num -= 1
