264550a26662379c908844f356b7192ad359f570
MennaEwas <61921636+MennaEwas@users.noreply.github.com>
2022-08-13 00:11:01 +0200

    Time: 908 ms (52.43%), Space: 14.8 MB (20.83%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -3,11 +3,11 @@ class Solution:
         longest, max_repeat, start = 0, 0, 0
         for end in range(len(s)):
             right = s[end]
-            if right == 1:
+            if right:
                 max_repeat += 1
             
             if (end - start - max_repeat + 1) > k:
-                if s[start] == 1:
+                if s[start]:
                     max_repeat -= 1
                 start += 1
                 
