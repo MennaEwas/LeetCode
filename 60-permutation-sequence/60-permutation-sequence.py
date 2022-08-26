bfecf9cbfe904703995cf03ea9507e8eac0b4266
MennaEwas <61921636+MennaEwas@users.noreply.github.com>
2022-08-26 19:13:34 +0200

    Time: 52 ms (51.78%), Space: 13.8 MB (99.07%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.py
+++ 60-permutation-sequence/60-permutation-sequence.py
@@ -1,5 +1,18 @@
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
-        permutations = list(itertools.permutations(range(1, n+1)))
-        return ''.join(map(str, permutations[k-1]))
+        #build permutions with memorization 
+        permutations = []
+        unused = list(range(1, n+1))
+        fact = [1]*(n+1) #base case 
+        for i in range(1, n+1):
+            fact[i] = i * fact[i-1]
+        k -= 1
+        while n > 0:
+            len_part = fact[n] // n
+            i = k//len_part
+            permutations.append(unused[i])
+            unused.pop(i)
+            n -= 1 
+            k %= len_part 
+        return ''.join(map(str, permutations))
         
