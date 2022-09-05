21eacdf6664bb571751349b24cc8a530d846fd97
MennaEwas <61921636+MennaEwas@users.noreply.github.com>
2022-09-05 20:54:36 +0200

    Time: 109 ms (55.52%), Space: 15 MB (63.82%) - LeetHub

---
+++ 1-lclighter-data-idlgt255811883-data-bundle-id0-stylebackground-image-linear-gradienttransparent-0-transparent-calc50-4px-rgb204-242-241-calc50-4px-rgb204-242-241-100-transition-background-position-120ms-ease-in-out-0s-padding-120ms-ease-in-out-0s-background-size-100-200-background-position-initial-user-select-auto-two-sum-lclighterdiv-classlinerthreadicon-linerfirst-lineroverlapse-data-highlight-id255811883-data-bundle-id0-idlgt255811883-stylebackground-image-url-and-quot-https-photogetlinercom-liner-service-bucket-user-photo-default-color-2-vsvg-and-quot-user-select-auto-div-classlinerthreadicon-dim-styleuser-select-auto-div-div-classlinerthreadicon-mentioned-styleuser-select-auto-div-classlinerthreadicon-mentionedimg-styleuser-select-auto-div-div-div-classlinerthreadicon-onlyme-styleuser-select-auto-div-classlinerthreadicon-onlymeimg-styleuser-select-auto-div-div-divdiv-classlinerthreadicon-linersecond-data-highlight-id257820954-data-bundle-id0-idlgt257820954-stylebackground-image-url-and-quot-https-photogetlinercom-liner-service-bucket-user-photo-default-color-5-gsvg-and-quot-user-select-auto-div-classlinerthreadicon-dim-styleuser-select-auto-div-div-classlinerthreadicon-mentioned-styleuser-select-auto-div-classlinerthreadicon-mentionedimg-styleuser-select-auto-div-div-div-classlinerthreadicon-onlyme-styleuser-select-auto-div-classlinerthreadicon-onlymeimg-styleuser-select-auto-div-div-div/1-lclighter-data-idlgt255811883-data-bundle-id0-stylebackground-image-linear-gradienttransparent-0-transparent-calc50-4px-rgb204-242-241-calc50-4px-rgb204-242-241-100-transition-background-position-120ms-ease-in-out-0s-padding-120ms-ease-in-out-0s-background-size-100-200-background-position-initial-user-select-auto-two-sum-lclighterdiv-classlinerthreadicon-linerfirst-lineroverlapse-data-highlight-id255811883-data-bundle-id0-idlgt255811883-stylebackground-image-url-and-quot-https-photogetlinercom-liner-service-bucket-user-photo-default-color-2-vsvg-and-quot-user-select-auto-div-classlinerthreadicon-dim-styleuser-select-auto-div-div-classlinerthreadicon-mentioned-styleuser-select-auto-div-classlinerthreadicon-mentionedimg-styleuser-select-auto-div-div-div-classlinerthreadicon-onlyme-styleuser-select-auto-div-classlinerthreadicon-onlymeimg-styleuser-select-auto-div-div-divdiv-classlinerthreadicon-linersecond-data-highlight-id257820954-data-bundle-id0-idlgt257820954-stylebackground-image-url-and-quot-https-photogetlinercom-liner-service-bucket-user-photo-default-color-5-gsvg-and-quot-user-select-auto-div-classlinerthreadicon-dim-styleuser-select-auto-div-div-classlinerthreadicon-mentioned-styleuser-select-auto-div-classlinerthreadicon-mentionedimg-styleuser-select-auto-div-div-div-classlinerthreadicon-onlyme-styleuser-select-auto-div-classlinerthreadicon-onlymeimg-styleuser-select-auto-div-div-div.py
@@ -0,0 +1,27 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        new = sorted(nums)
+        i = 0
+        j = len(nums) - 1
+        ls = []
+        while j > i:
+            l = new[i]
+            m = new[j]
+            k = l + m 
+            if k == target:
+                ls.append(nums.index(l))
+                nums[nums.index(l)] = '_'
+                ls.append(nums.index(m))
+                return ls
+            else: 
+                if k > target:
+                    j-=1 
+                else:
+                    i+=1
+                
+                    
+                    
+                        
+            
+        
+        
